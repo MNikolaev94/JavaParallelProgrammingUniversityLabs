53ba523ac03d36f437c0df9dbee75f1a5d5134df
MNikolaev94 <mars-2008-94@mail.ru>
2017-05-11 16:53:32 +0300

    Add files via upload

---
+++ Blab1/src/main/java/ru/spbstu/telematics/java/lab2/Montekarlo.java
@@ -0,0 +1,54 @@
+package ru.spbstu.telematics.java.lab2;
+
+public class Montekarlo {
+	static int nCircle1; //Количество точек, попавших в круг
+	Montekarlo(){
+		
+		nCircle1 = 0;
+	}
+    public static void main( String[] args )
+    {
+    	double pi = 0;    	
+    	int maxThread = 1;
+    	double rCircle = 1.0; //Радиус круга
+    	int n = 90000; //Общее количество точек
+    	//Если количество потоков больше количества точек, то уменьшаем количество потоков
+    	while(maxThread != 4){
+    		if(maxThread > n)
+    			maxThread = n;
+    	// посчитаем сколько точек "забросит" каждый поток
+    		int count = n/maxThread;
+    		int additional = n % maxThread; // если не делится на maxThread, то добавим к первому потоку
+
+    		Thread[] threads = new Thread[maxThread];
+    		int start = 0;
+    		for(int i = 0; i < maxThread; i++)
+    		{
+    			int cnt = ( i == 0 ? count + additional : count );
+    			threads[i] = new MyThreadCircle(rCircle, start, start + cnt - 1);
+    			start += cnt;
+    			threads[i].start();
+    		}
+ 
+    		try{
+
+    			long timeStart = System.currentTimeMillis();
+
+    			for(Thread thread : threads)
+    				thread.join();
+    			long timeEnd = System.currentTimeMillis();
+    			System.out.println("Количество потоков " + maxThread + "; Время:" + Math.abs(timeStart - timeEnd));
+          //  System.out.println("Точек в круге " + nCircle1);
+    			pi = (double)4*nCircle1/n;
+    			System.out.println("Число ПИ вычесленное методом Монте - Карло равно = " + pi);
+    			maxThread++;
+            	nCircle1 = 0;
+    		} 
+    		catch(InterruptedException e) {
+    			System.out.println("Interrupted");
+    		}
+    		
+    	}
+}
+
+}
